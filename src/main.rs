dfe0a26fcb66635fd8a772a886ff65ee32a8fe74
Andrew Muchangi <andrew@Muchangi.>
2025-04-26 12:28:27 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
+struct Person {
+    name: String,
+    edges: Vec<Edge>,
+}
+
+#[derive(Debug, Clone)]
 struct Metadata {
     to: String,
     label: String,
     timestamp: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Edge {
     metadata: Metadata,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Graph {
-    adj_list: HashMap<String, Vec<Edge>>,
+    adj_list: HashMap<String, Person>,
 }
 
 impl Graph {
@@ -24,20 +30,20 @@ impl Graph {
         }
     }
 
-    fn add_node(&mut self, n: &str) {
-        self.adj_list.entry(n.to_string()).or_insert(vec![]);
+    fn add_node(&mut self, p: Person) {
+        self.adj_list.entry(p.name.to_string()).or_insert(p);
     }
 
-    fn add_edge(&mut self, src: &str, md: Metadata) {
-        if let Some(v) = self.adj_list.get_mut(src) {
-            v.push(Edge { metadata: md });
+    fn add_edge(&mut self, src: Person, md: Metadata) {
+        if let Some(p) = self.adj_list.get_mut(&src.name) {
+            p.edges.push(Edge { metadata: md});
         }
     }
 
     fn display(&self) {
-        for (node, edges) in &self.adj_list {
-            print!("Node({}) => ", node);
-            for edge in edges {
+        for (id, node) in &self.adj_list {
+            print!("Node({}) => ", id);
+            for edge in &node.edges {
                 println!(
                     "Edge(to -> {}, label : {}, timestamp : {})",
                     edge.metadata.to, edge.metadata.label, edge.metadata.timestamp
@@ -50,35 +56,44 @@ impl Graph {
 fn main() {
     let mut g = Graph::new();
 
-    let n1 = "A";
-    let n2 = "B";
-    let n3 = "C";
+    let n1 = Person {
+        name: "John".to_string(),
+        edges: vec![],
+    };
+    let n2 = Person {
+        name: "Jane".to_string(),
+        edges: vec![],
+    };
+    let n3 = Person {
+        name: "Alice".to_string(),
+        edges: vec![],
+    };
 
-    g.add_node(n1);
-    g.add_node(n2);
-    g.add_node(n3);
+    g.add_node(n1.clone());
+    g.add_node(n2.clone());
+    g.add_node(n3.clone());
 
     g.add_edge(
-        n1,
+        n1.clone(),
         Metadata {
-            to: n2.to_string(),
-            label: "wife".to_string(),
+            to: n2.name.to_string(),
+            label: "husband of".to_string(),
             timestamp: "01/01/1980".to_string(),
         },
     );
     g.add_edge(
-        n2,
+        n2.clone(),
         Metadata {
-            to: n3.to_string(),
-            label: "daughter".to_string(),
+            to: n3.name.to_string(),
+            label: "mother of".to_string(),
             timestamp: "01/01/2000".to_string(),
         },
     );
     g.add_edge(
-        n3,
+        n3.clone(),
         Metadata {
-            to: n1.to_string(),
-            label: "father".to_string(),
+            to: n1.name.to_string(),
+            label: "daughter of".to_string(),
             timestamp: "01/01/2000".to_string(),
         },
     );
